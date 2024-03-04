41ce5d79304f2fcae2d4f796b4909f2926467b2b
jhansiA64 <jhansi.a64@gmail.com>
2024-03-04 05:36:33 +0000

    04032024

---
+++ orc/orc/doctype/transaction_details/transaction_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, jhansi64 and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class TransactionDetails(Document):
+	pass
